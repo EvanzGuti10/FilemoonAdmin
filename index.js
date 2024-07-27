a76b34fe103840a6909089b90edbbd4d7988ee14
EvanzGuti10 <116958095+EvanzGuti10@users.noreply.github.com>
2024-07-26 22:04:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,33 +5,7 @@ var ienlaces = document.getElementById('enlaces');
 var boton = document.getElementById("btnenviar");
 const dataArray = [];
 
-//const urlante = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSKuuCNbkpOGNDcVetXOCcJ_I-WVUKeKqyeFNge61-xZyVeLTakn57b_sGtQESpX5RjTpcJtw0bTtC1/pubhtml?gid=0&single=true';
 const urlfile = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQcHf31D-1ci81iVfKNpa_Bu-DxgA0I6FsL1QZswGwJXYmyjM0uQRkJHchb7R2JwedjAlBO_sHR-nfF/pubhtml?gid=0&single=true';
-//const urlwish = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKxE5fsOIBX0XEq-Fd-noZL9O6KTSb4hwT_TuaoTM-3DpKXe2W1owqPa0ph30wfyCLEBjcDWF_3Csj/pubhtml?gid=0&single=true';
-
-/*fetch(urlante)
-    .then(response => response.text())
-    .then(data => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(data, 'text/html');
-        const table = doc.querySelector('table');
-        const rows = table.querySelectorAll('tr');
-        let rowIndex = 0;
-        rows.forEach(row => {
-            if (rowIndex >= 2) {
-                const rowData = [];
-                row.querySelectorAll('td').forEach(cell => {
-                    rowData.push(cell.innerText);
-                });
-                dataArray.push(rowData);
-            }
-
-            rowIndex++;
-        });
-    })
-    .catch(error => {
-        console.log('Error al obtener los datos:', error);
-    });*/
 
 function btnLimpiar() {
     //Cuando se presione el boton limpiar se mandara al enlace del servidor
@@ -51,33 +25,23 @@ function btnLimpiar() {
         .catch(error => console.error(error));
 }
 
-function btnActualizar() {
-    fetch(urlfile)
-    //fetch(urlwish)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const doc = parser.parseFromString(data, 'text/html');
-            const table = doc.querySelector('table');
-            const rows = table.querySelectorAll('tr');
-
-            const cantidadFilas = rows.length - 1;
-
-            inombre.value = '' + dataArray[cantidadFilas][0];
-            iimagen.value = '' + dataArray[cantidadFilas][1];
-        })
-        .catch(error => {
-            console.log('Error al obtener los datos:', error);
-        });
-}
-
 function btnCrear() {
-    var n = inombre.value;
-    var imagen = iimagen.value;
-    var nombre = encodeURIComponent(n);
+    var n_i = inombre.value;
+    //var nombre_imagen = encodeURIComponent(n);
     var codigo = '';
+
+    //Obteniendo el nombre y la imagen
+    var datos = n_i.split('→');
+    var nombre = encodeURIComponent(datos[0]);
+    var urlImagen = datos[1];
+
+    //Url para crear la carpeta
     var urlc = 'https://filemoonapi.com/api/folder/create?key=54340gjpnv8a0abxcv6s4&name=' + nombre;
-    //var urlc = 'https://api.streamwish.com/api/folder/create?key=3609fo97wn3tj97h60t5&name=' + nombre;
+
+    //Mandando el nombre al campo imagen
+    imagen.value = urlImagen;
+    var imagenSubir = imagen.value;
+
     fetch(urlc)
         .then(response => response.json())
         .then(data => {
@@ -86,7 +50,7 @@ function btnCrear() {
 
             var urle = 'https://script.google.com/macros/s/AKfycbweUyIoRlZXsrDL95366BoYbPMmkozy0n6sglQf8lhGI2zIL4Wug3qIHb3Cctu6nSvW/exec?';
 
-            fetch(urle + 'nombre=' + nombre + '&imagen=' + imagen + '&codigo=' + codigo)
+            fetch(urle + 'nombre=' + nombre + '&imagen=' + urlImagen + '&codigo=' + codigo)
                 .then(response => response.json())
                 .then(data => {
                     console.log('Exitoso');
@@ -105,7 +69,6 @@ function btnEnviar() {
     for (var i = 0; i < lineas.length; i++) {
         if (lineas[i][0] == 'h') {
             var url = 'https://filemoonapi.com/api/remote/add?key=54340gjpnv8a0abxcv6s4&fld_id=' + codigo + "&url=";
-            //var url = 'https://api.streamwish.com/api/upload/url?key=3609fo97wn3tj97h60t5&fld_id=' + codigo + "&url=";
             var subir = url + lineas[i];
 
             fetch(subir)
